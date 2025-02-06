023a861d4aaa85e39d58ac26058b461504d5def4
Yannick Remy <remyyannick@sfr.fr>
2025-02-06 20:40:27 +0100

    refactor: réorganisation de la structure JSX du composant `Navbar`

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,6 @@ const navLinks: { href: string; text: string }[] = [
   { href: "#about", text: "à propos" },
   { href: "#skills", text: "compétences" },
   { href: "#projects", text: "projets" },
-  { href: "#contact", text: "contact" },
 ] as const;
 
 const Navbar: FC = () => {
@@ -16,19 +15,21 @@ const Navbar: FC = () => {
   return (
     <header>
       <nav>
+        <a href="#">logo</a>
+        <ul>
+          {navLinks.map(({ href, text }, index) => (
+            <li
+              key={index}
+              ref={(el) => {
+                navLinkRefs.current[index] = el!;
+              }}
+            >
+              <a href={href}>{text}</a>
+            </li>
+          ))}
+        </ul>
         <div>
-          <ul>
-            {navLinks.map(({ href, text }, index) => (
-              <li
-                key={index}
-                ref={(el) => {
-                  navLinkRefs.current[index] = el!;
-                }}
-              >
-                <a href={href}>{text}</a>
-              </li>
-            ))}
-          </ul>
+          <a href="#">Contact</a>
         </div>
       </nav>
     </header>
